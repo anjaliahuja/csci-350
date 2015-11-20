146e3f936e73b008846f6640f63d91af16c5bae4
Anne Kao <annekao@aludra.usc.edu>
2015-11-19 17:39:39 -0800

    manager done

--- code/test/Manager.c
+++ code/test/Manager.c
@@ -1,10 +1,11 @@
 #include "syscall.h"
 #include "Setup.h"
+#include <string>
 
-void managerWakeup(int clerk, String clerkType) {
+void managerWakeup(int clerk, std::string clerkType) {
   int lock, cv;
-  lock = GetMV(clerk, 2);
-  cv = GetMV(clerk, 3);
+  lock = GetMV(clerk, Lock);
+  cv = GetMV(clerk, CV);
 
   Acquire(lock);
   Signal(lock, cv);
@@ -14,7 +15,7 @@ void managerWakeup(int clerk, String clerkType) {
 }
 
 void startManager() {
-  int i, j, currClerkI, currClerkJ, lineSizeI, lineSizeJ, state;
+  int i, j, currClerkI, currClerkJ, lineSizeI, lineSizeJ, state, total;
 
 
   while(true) {
@@ -22,11 +23,11 @@ void startManager() {
       /*If the clerk is on break, aka their state is 2 and their line has more than 3 people
       Wake up the thread*/
       currClerkI = GetMV(appClerks, i);
-      lineSizeI = GetMV(currClerkI, ACCount)
+      lineSizeI = GetMV(currClerkI, Count)
       if(lineSizeI > 2) {
           for(j = 0; j < NUM_APPCLERKS; j++) {
             currClerkJ = GetMV(appClerks, j);
-            state = GetMV(currClerkJ, ACState);
+            state = GetMV(currClerkJ, State);
 
             if(state == ONBREAK) {
               managerWakeup(currClerkJ, "ApplicationClerk");
@@ -38,8 +39,8 @@ void startManager() {
       if (i == NUM_APPCLERKS-1) {
         for(j = 0; j < NUM_APPCLERKS; j++) {
           currClerkJ = GetMV(appClerks, j);
-          state = GetMV(currClerkJ, ACState);
-          lineSizeJ = GetMV(currClerkJ, ACCount);
+          state = GetMV(currClerkJ, State);
+          lineSizeJ = GetMV(currClerkJ, Count);
 
           if (lineSizeJ > 0 && state == ONBREAK) {
             managerWakeup(currClerkJ, "ApplicationClerk");
@@ -50,14 +51,14 @@ void startManager() {
 
     for(i = 0; i < NUM_PICCLERKS; i++) {
       currClerkI = GetMV(picClerks, i);
-      lineSizeI = GetMV(currClerkI, ACCount)
+      lineSizeI = GetMV(currClerkI, Count)
       if(lineSizeI) > 2) {
           for(j = 0; j < NUM_PICCLERKS; j++) {
             currClerkJ = GetMV(picClerks, j);
-            state = GetMV(currClerkJ, ACState);
+            state = GetMV(currClerkJ, State);
 
             if(state == ONBREAK) {
-              managerWakeupPicClerk(currClerkJ, "PictureClerk");
+              managerWakeup(currClerkJ, "PictureClerk");
             }
           }
           break;
@@ -66,15 +67,120 @@ void startManager() {
       if (i == NUM_PICCLERKS-1) {
         for(j = 0; j < NUM_PICCLERKS; j++) {
           currClerkJ = GetMV(appClerks, j);
-          state = GetMV(currClerkJ, ACState);
-          lineSizeJ = GetMV(currClerkJ, ACCount);
+          state = GetMV(currClerkJ, State);
+          lineSizeJ = GetMV(currClerkJ, Count);
           if (lineSizeJ > 0 && state == ONBREAK) {
-              managerWakeupPicClerk(currClerkJ, "PictureClerk");
+              managerWakeup(currClerkJ, "PictureClerk");
           }
         }
       }
     }
+
+    for(i = 0; i < NUM_PASSPORTCLERKS; i++) {
+      currClerkI = GetMV(passportClerks, i);
+      lineSizeI = GetMV(currClerkI, Count)
+      if(lineSizeI > 2) {
+          for(j = 0; j < NUM_PASSPORTCLERKS; j++) {
+            currClerkJ = GetMV(passportClerks, j);
+            state = GetMV(currClerkJ, State);
+            if(state == ONBREAK) {
+              managerWakeup(currClerkJ, "PassportClerk");
+            }
+          }
+          break;
+        }
+
+      if (i == NUM_PASSPORTCLERKS-1) {
+        for(j = 0; j < NUM_PASSPORTCLERKS; j++) {
+          currClerkJ = GetMV(passportClerks, j);
+          state = GetMV(currClerkJ, State);
+          lineSizeJ = GetMV(currClerkJ, Count);
+          if (lineSizeJ > 0 && state == ONBREAK) {
+            managerWakeup(currClerkJ, "PassportClerk");
+          }
+        }
+      }
+    }
+
+    for(i = 0; i < NUM_CASHIERS; i++) {
+      currClerkI = GetMV(cashiers, i);
+      lineSizeI = GetMV(currClerkI, Count)
+      if(lineSizeI > 2) {
+        for(j = 0; j < NUM_CASHIERS; j++) {
+          currClerkJ = GetMV(cashiers, j);
+          state = GetMV(currClerkJ, State);
+          if(state == ONBREAK) {
+            managerWakeup(currClerkJ, "Cashier");
+          }
+        }
+        break;
+      }
+
+      if (i == NUM_CASHIERS-1) {
+        for(j = 0; j < NUM_CASHIERS; j++) {
+          currClerkJ = GetMV(cashiers, j);
+          state = GetMV(currClerkJ, State);
+          lineSizeJ = GetMV(currClerkJ, Count);
+          if (lineSizeJ > 0 && state == 2) {
+            managerWakeup(currClerkJ, "Cashier");
+          }
+        }
+      }
+    }
+  
+    for(i = 0; i < 500; i++) {
+      Yield();
+    }
+        /* no more customers */
+    if (numCustomers == 0) {
+      for(j = 0; j < NUM_APPCLERKS; j++) {
+        currClerkJ = GetMV(appClerks, j);
+        state = GetMV(currClerkJ, State);
+        if(state == ONBREAK) {
+          managerWakeup(currClerkJ, "ApplicationClerk");
+        }
+      }
+      for(j = 0; j < NUM_PICCLERKS; j++) {
+        currClerkJ = GetMV(picClerks, j);
+        state = GetMV(currClerkJ, State);
+        if (state == ONBREAK) {
+          managerWakeup(currClerkJ, "PictureClerk");
+        }
+      }
+      for(j = 0; j < NUM_PASSPORTCLERKS; j++) {
+        currClerkJ = GetMV(passportClerks, j);
+        state = GetMV(currClerkJ, State);
+        if (state == ONBREAK) {
+          managerWakeup(currClerkJ, "PassportClerk");
+        }
+      }
+      for(j = 0; j < NUM_CASHIERS; j++) {
+        currClerkJ = GetMV(cashiers, j);
+        state = GetMV(currClerkJ, State);
+        if (state == ONBREAK) {
+          managerWakeup(currClerkJ, "Cashier");
+        }
+      }
+
+      total = GetMV(AppClerkBribeMoney, 0) + GetMV(PicClerkBribeMoney,0) + GetMV(PassportClerkBribeMoney,0) + GetMV(CashierMoney,0);
+
+      Printf("Manager has has counted a total of %d for Application Clerks \n", sizeof("Manager has has counted a total of %d for Application Clerks \n"), GetMV(AppClerkBribeMoney, 0));
+      Printf("Manager has has counted a total of %d for Picture Clerks \n", sizeof("Manager has has counted a total of %d for Picture Clerks \n"), GetMV(PicClerkBribeMoney,0));
+      Printf("Manager has has counted a total of %d for Passport Clerks \n", sizeof("Manager has has counted a total of %d for Passport Clerks \n"), GetMV(PassportClerkBribeMoney,0));
+      Printf("Manager has has counted a total of %d for Cashiers \n", sizeof("Manager has has counted a total of %d for Cashiers\n"), GetMV(CashierMoney,0));
+      Printf("Manager has has counted a total of %d for the Passport Office \n", sizeof("Manager has has counted a total of %d for the Passport Office \n"), total);
+      
+      break;
+    }
+    total = GetMV(AppClerkBribeMoney, 0) + GetMV(PicClerkBribeMoney,0) + GetMV(PassportClerkBribeMoney,0) + GetMV(CashierMoney,0);
+
+    Printf("Manager has has counted a total of %d for Application Clerks \n", sizeof("Manager has has counted a total of %d for Application Clerks \n"), GetMV(AppClerkBribeMoney, 0));
+    Printf("Manager has has counted a total of %d for Picture Clerks \n", sizeof("Manager has has counted a total of %d for Picture Clerks \n"), GetMV(PicClerkBribeMoney,0));
+    Printf("Manager has has counted a total of %d for Passport Clerks \n", sizeof("Manager has has counted a total of %d for Passport Clerks \n"), GetMV(PassportClerkBribeMoney,0));
+    Printf("Manager has has counted a total of %d for Cashiers \n", sizeof("Manager has has counted a total of %d for Cashiers\n"), GetMV(CashierMoney,0));
+    Printf("Manager has has counted a total of %d for the Passport Office \n", sizeof("Manager has has counted a total of %d for the Passport Office \n"), total);
   }
+  Exit(0);
 }
 
 int main() {
